dfcbf1c1bc76a153a14a66b914531c161e168ef8
MERLIN21B <sidbhardwj21b@gmail.com>
2022-06-11 01:28:37 +0530

    master

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+let mysql = require('mysql');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var studentRouter = require('./routes/student');
+var teacherRouter = require('./routes/teacher');
+
 var app = express();
-
 // view engine setup
+// app.use(express.static('views',{extensions:['html']}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
+app.set('view engine', 'ejs');
+// app.engine('html',require('hogan-express'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/student', studentRouter);
+app.use('/teacher', teacherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-   res.render('Index');
-   // console.log(req)
-   // res.render( { name: 'sidhartha bhardwaj' });
-
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "data"
 });
-router.get('/sid', function(req, res, next) {
-   res.json({name:req.query.name,age:req.query.contact});
- });
- router.post('/data', function(req, res, next) {
-   res.json({name:req.body.name,age:req.body.contact});
- });
+
+router.get('/', function(req, res, next) {
+    con.connect(function(err) {
+        if (err) console.log("err");
+        console.log("Connected to mySQL!");
+      });
+    res.render('index')
+});
+
+router.post('/about', function(req, res, next) {
+    console.log(req.body)
+    var sql = "INSERT INTO `form_details` (`name`, `country`, `age`) \
+    VALUES ('"+req.body.name+"', '"+req.body.country+"', '"+req.body.age+"');"
+    console.log(sql)
+   con.connect()
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+
+      });
+      var sql1 = "select * from `form_details`"
+      con.query(sql1, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+res.json({"Name":req.body})
+});
+
+router.get('/where', function(req, res, next) {
+  //   res.send('eraf');
+    console.log(req)
+    res.json({name:"eraf"})
+  });
 
 module.exports = router;
